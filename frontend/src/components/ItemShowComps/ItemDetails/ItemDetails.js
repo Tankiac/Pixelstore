3d57d18f1590478e084bd10f20330ab69d13a05d
Tankiac <tarikov.mail@gmail.com>
2021-07-28 22:01:25 +0200

    fixed delivery date bug

--- frontend/src/components/ItemShowComps/ItemDetails/ItemDetails.js
+++ frontend/src/components/ItemShowComps/ItemDetails/ItemDetails.js
@@ -23,9 +23,40 @@ const ItemDetails = (props) => {
     const timeConverter = (offset = 0) => {
         let unix = new Date(Date.now());
         let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+        let months31 = ["Jan", "Mar", "May", "Jul", "Sep", "Nov"];
+        let months30 = ["Apr", "Jun", "Aug", "Oct", "Dec"]; 
         let month = months[unix.getMonth()];
         let date = unix.getDate();
-        let time = `${month} ${date + offset}.`
+        if (months31.includes(month)) {
+            if (date + offset > 31) {
+                if (month === "Dec") {
+                    month = "Jan"
+                } else {
+                    month = months[months.indexOf(month) + 1]
+                }
+                date = date + offset - 31;
+            } else {
+                date += offset;
+            }
+        } else if (months30.includes(month)) {
+            if (date + offset > 30) {
+                if (month === "Dec") {
+                    month = "Jan"
+                } else {
+                    month = months[months.indexOf(month) + 1]
+                }
+                date = date + offset - 31;
+            } else {
+                date += offset;
+            }
+        } if (month === "Feb") {
+            if (date + offset > 28) {
+                    month = months[months.indexOf(month) + 1] 
+            } else {
+                date += offset;
+            }
+        }
+        let time = `${month} ${date}.`
         return time;
       }
 
